fb49cd34983b91fc09f9b01c0d15adb3fd014211
FRANCONIERI Jean-Pierre - m501657 <jean-pierre.franconieri@soprasteria.com>
2018-08-20 10:57:43 +0200

    use jsx syntax

--- src/TimeGrid.js
+++ src/TimeGrid.js
@@ -5,8 +5,8 @@ const styles = {
   container: {
     position: "relative",
     fontFamily: "'Roboto', sans-serif",
-    fontSize: 12,
-},
+    fontSize: 12
+  },
   legend: {
     position: "absolute",
     color: "grey"
@@ -18,9 +18,86 @@ const styles = {
   },
   bar: {
     position: "absolute",
-    border: '1px solid darkgrey',
-  },
+    border: "1px solid darkgrey"
+  }
 };
+
+function Tick(props) {
+  const tickStyle = {
+    ...styles.legend,
+    left: props.x,
+    top: props.y
+  };
+  return (
+    <div style={tickStyle}>
+      <span>{props.tick.toString().padStart(2, "0") + ":00"}</span>
+    </div>
+  );
+}
+
+function Ticks(props) {
+  let ticks = [];
+  for (let tick = props.start; tick < props.end; tick++) {
+    ticks.push(
+      <Tick
+        key={"tick-" + tick.toString()}
+        tick={tick}
+        x={props.x + (tick - props.start) * props.tickLength}
+        y={props.y}
+      />
+    );
+  }
+  return ticks;
+}
+
+function TickLines(props) {
+  let ticks = [];
+  for (let tick = props.start; tick < props.end; tick++) {
+    ticks.push(
+      <TickLine
+        key={"tickline-" + tick.toString()}
+        x={15 + props.x + (tick - props.start) * props.tickLength}
+        y={props.y}
+      />
+    );
+  }
+  return ticks;
+}
+
+function TickLine(props) {
+  const tickLineStyle = {
+    ...styles.tickLine,
+    left: props.x,
+    top: props.y
+  };
+  return <div style={tickLineStyle} />;
+}
+
+function EmptyBars(props) {
+  let bars = [];
+  for (let tick = props.start; tick < props.end + 1; tick++) {
+    bars.push(
+      <EmptyBar
+        key={"emptybar-" + tick.toString()}
+        x={15 + props.x + (tick - props.start) * props.tickLength}
+        y={props.y}
+        width={props.tickLength}
+      />
+    );
+  }
+  return bars;
+}
+
+function EmptyBar(props) {
+  const barStyle = {
+    ...styles.bar,
+    left: props.x,
+    top: props.y,
+    height: "auto",
+    width: props.width
+  };
+  return <div style={barStyle}>&nbsp;</div>;
+}
 
 export default class TimeGrid extends Component {
   static defaultProps = {
@@ -42,107 +119,33 @@ export default class TimeGrid extends Component {
     return (
       <div style={styles.container}>
         <div>
-          {this.createAllTicks({
-            start: this.props.start,
-            end: this.props.end,
-            x: this.props.x + this.props.tickLength,
-            y: this.props.y,
-            tickLength: this.props.tickLength
-          })}
+          <Ticks
+            start={this.props.start}
+            end={this.props.end}
+            x={this.props.x + this.props.tickLength}
+            y={this.props.y}
+            tickLength={this.props.tickLength}
+          />
         </div>
         <div>
-          {this.createAllTickLines({
-            start: this.props.start,
-            end: this.props.end,
-            x: this.props.x + this.props.tickLength,
-            y: this.props.y + 18,
-            tickLength: this.props.tickLength
-          })}
+          <TickLines
+            start={this.props.start}
+            end={this.props.end}
+            x={this.props.x + this.props.tickLength}
+            y={this.props.y + 18}
+            tickLength={this.props.tickLength}
+          />
         </div>
         <div>
-          {this.createAllEmptyBars({
-            start: this.props.start,
-            end: this.props.end,
-            x: this.props.x,
-            y: this.props.y + 36 - 8,
-            tickLength: this.props.tickLength
-          })}
+          <EmptyBars
+            start={this.props.start}
+            end={this.props.end}
+            x={this.props.x}
+            y={this.props.y + 36 - 8}
+            tickLength={this.props.tickLength}
+          />
         </div>
       </div>
     );
   }
-
-  createAllTicks = props => {
-    let ticks = [];
-    for (let tick = props.start; tick < props.end; tick++) {
-      ticks.push(
-        this.createTick({
-          tick: tick,
-          x: props.x + (tick - props.start) * props.tickLength,
-          y: props.y
-        })
-      );
-    }
-    return ticks;
-  };
-
-  createTick = props => {
-    const tickStyle = {
-      ...styles.legend,
-      left: props.x,
-      top: props.y
-    };
-    return (
-      <div style={tickStyle}>
-        <span>{props.tick.toString().padStart(2, "0") + ":00"}</span>
-      </div>
-    );
-  };
-
-  createAllTickLines = props => {
-    let ticks = [];
-    for (let tick = props.start; tick < props.end; tick++) {
-      ticks.push(
-        this.createTickLine({
-          x: 15 + props.x + ((tick - props.start) * props.tickLength),
-          y: props.y
-        })
-      );
-    }
-    return ticks;
-  };
-
-  createTickLine = props => {
-    const tickLineStyle = {
-      ...styles.tickLine,
-      left: props.x,
-      top: props.y
-    };
-    return <div style={tickLineStyle} />;
-  };
-
-  createAllEmptyBars = props => {
-    let bars = [];
-    for (let tick = props.start; tick < props.end + 1; tick++) {
-      bars.push(
-        this.createEmptyBar({
-          x: 15 + props.x + ((tick - props.start) * props.tickLength),
-          y: props.y,
-          width: props.tickLength,
-        })
-      );
-    }
-    return bars;
-  };
-
-  createEmptyBar = props => {
-    const barStyle = {
-      ...styles.bar,
-      left: props.x,
-      top: props.y,
-      height: 'auto', 
-      width: props.width,
-    };
-    return <div style={barStyle}>&nbsp;</div>;
-  };
 }

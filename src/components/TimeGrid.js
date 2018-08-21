80f793490d3e986fd25065f7941b516c0baf517b
FRANCONIERI Jean-Pierre - m501657 <jean-pierre.franconieri@soprasteria.com>
2018-08-21 20:34:16 +0200

    added scale

--- src/components/TimeGrid.js
+++ src/components/TimeGrid.js
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 const GRIDCELL_WIDTH = 48;
 
 export default class TimeGrid extends Component {
-
   static propTypes = {
     start: PropTypes.number,
     end: PropTypes.number
@@ -64,18 +63,57 @@ export default class TimeGrid extends Component {
           };
         }
         return { ...style, ...firstChildStyle, ...lastChildStyle };
+      },
+
+      scale: {
+        display: "table",
+        borderSpacing: 0,
+        border: "1px solid transparent"
+      },
+
+      scaleRow: {
+        display: "table-row"
+      },
+
+      scaleCell: (isFirstChild, isLastChild) => {
+        let style = {
+          display: "table-cell",
+          width: GRIDCELL_WIDTH,
+          minWidth: GRIDCELL_WIDTH,
+          height: "auto",
+          borderLeft: "1px solid transparent",
+          borderRight: "1px solid transparent",
+          textAlign: 'left'
+        };
+        if (isFirstChild) {
+          style.width = 20;
+          style.minWidth = 20;
+        }
+        return style;
       }
     };
 
-    const GridCell = props => {
-      return (<div style={styles.gridCell(props.isFirstChild, props.isLastChild)}>{props.children}</div>)
-    }
+   const GridCell = props => {
+      return (
+        <div style={styles.gridCell(props.isFirstChild, props.isLastChild)}>
+          {props.children}
+        </div>
+      );
+    };
 
     const Grid = props => {
       let cells = [];
       const cellCount = 1 + props.end - props.start + 1;
       for (let cell = 0; cell < cellCount; cell++) {
-        cells.push(<GridCell key={'grid-cell#' + cell.toString()} isFirstChild={cell === 0} isLastChild={cell === cellCount -1}>&nbsp;</GridCell>)
+        cells.push(
+          <GridCell
+            key={"grid-cell#" + cell.toString()}
+            isFirstChild={cell === 0}
+            isLastChild={cell === cellCount - 1}
+          >
+            &nbsp;
+          </GridCell>
+        );
       }
       return (
         <div style={styles.grid}>
@@ -84,8 +122,39 @@ export default class TimeGrid extends Component {
       );
     };
 
+    const ScaleCell = props => {
+      return (
+        <div style={styles.scaleCell(props.isFirstChild, props.isLastChild)}>
+          <span style={props.isFirstChild ? {} : {marginLeft: -16}}>
+            {props.children}
+          </span>
+        </div>
+      );
+    };
+
+     const Scale = props => {
+      let cells = [];
+      for (let hour = props.start - 1; hour <= props.end; hour++) {
+        cells.push(
+          <ScaleCell
+            key={"grid-scale#" + hour.toString()}
+            isFirstChild={hour < props.start}
+            isLastChild={!(hour < props.end)}
+          >
+            {hour < props.start ? "" : hour.toString().padStart(2, "0") + ":00"}
+          </ScaleCell>
+        );
+      }
+      return (
+        <div style={styles.scale}>
+          <div style={styles.scaleRow}>{cells}</div>
+        </div>
+      );
+    };
+
     return (
       <div style={styles.container}>
+        <Scale start={this.props.start} end={this.props.end} />
         <Grid start={this.props.start} end={this.props.end} />
       </div>
     );
